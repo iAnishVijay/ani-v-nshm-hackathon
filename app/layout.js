37d987e1b72b9bd1a6286ec2bd16846ddf529246
Anish Vijay <anishvijay333@gmail.com>
2023-09-15 16:41:52 +0530

    code refactor and final commit

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import './globals.css'
 import { Poppins } from 'next/font/google'
 
 
-const inter = Poppins({ weight: '400', subsets: ['latin'] })
+const poppins = Poppins({ weight: '400', subsets: ['latin'] })
 
 export const metadata = {
   title: 'AGC Conference Activities',
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   )
 }
